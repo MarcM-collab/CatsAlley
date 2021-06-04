521e28cbfcc35740c31147dcc192cde3e61d0a6b
AinoaTA <atabares@edu.tecnocampus.cat>
2021-06-04 02:52:46 +0200

    fade y cartas

--- Assets/Scripts/CustomSceneManager.cs
+++ Assets/Scripts/CustomSceneManager.cs
@@ -8,12 +8,16 @@ public class CustomSceneManager : MonoBehaviour
     public static CustomSceneManager SceneManagerCustom;
     public bool test = false;
     private int levelsUnlocked = 0;
+
+
     public int GetLevelsUnlocked()
     {
         return levelsUnlocked;
     }
     private void Awake()
     {
+     
+
         if (SceneManagerCustom != null)
         {
             Destroy(gameObject);
@@ -39,16 +43,29 @@ public class CustomSceneManager : MonoBehaviour
     }
     public void LoadScene(int buildIndex)
     {
-        SceneManager.LoadScene(buildIndex);
+        Fade();
+        StartCoroutine(Load(buildIndex));
     }
 
+    private IEnumerator Load(int buildIndex)
+    {
+        yield return new WaitForSeconds(0.75f);
+        SceneManager.LoadScene(buildIndex);
+    }
+    private void Fade()
+    {
+        Animator fade = GameObject.FindGameObjectWithTag("Fade").GetComponent<Animator>();
+        fade.SetBool("FadeOut", true);
+    }
     private void OnLevelWasLoaded(int level)
     {
+
         if (level != 0) //menu
         {
             Init();
         }
     } 
+
     public void Init()
     {
         EntityManager.InitEntities();
