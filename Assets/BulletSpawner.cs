d87d3686d85fe3af34e1d280af795907a017047c
MarcM-collab <58913562+MarcM-collab@users.noreply.github.com>
2021-06-08 21:31:12 +0200

    Bullet + ligh anim

--- Assets/BulletSpawner.cs
+++ Assets/BulletSpawner.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class BulletSpawner : MonoBehaviour
 {
     public GameObject bullet;
-
+    private const float waitToSpawn = 0.35f;
     public void Spawn()
     {
+        StartCoroutine(DelaySpawn());
+    }
+    private IEnumerator DelaySpawn()
+    {
+        yield return new WaitForSeconds(waitToSpawn);
         var bulletInstance = Instantiate(bullet, EntityManager.ExecutorCharacter.transform, EntityManager.ExecutorCharacter.transform);
         var bulletComponent = bulletInstance.GetComponent<Bullet>();
         bulletComponent.InitialPosition = transform.position;
